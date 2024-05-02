90c2e58cc6e00238e99296888ccead7ea59b4e0d
Brian NQC <brian.nqc2023@gmail.com>
2024-05-02 09:16:04 +0700

    [GitHub Actions] Trigger manual workflow with REST API

---
+++ github-actions/trigger-workflow_dispatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OWNER=briannqc
+REPO=learning-hub
+WORKFLOW_ID=manually-triggered-workflow.yaml
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$WORKFLOW_ID/dispatches \
+  -d '{
+    "ref": "main",
+    "inputs": {
+        "string": "Brian NQC",
+        "number": "2023",
+        "option": "Option 1",
+        "boolean": true,
+        "environment": "prod"
+      }
+    }'
